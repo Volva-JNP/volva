2d26e7784cbe723ba9e1d23dab71b674fdbbe51e
Volva-JNP <100067154+Volva-JNP@users.noreply.github.com>
2022-02-20 10:08:15 +0100

    Add files via upload

---
+++ volva_text.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb 19 19:03:22 2022
+
+@author: jk
+"""
+
+path = 'volva_datas_utlimate_one.csv'
+path_brut = 'volumesMARS2021.csv'
+
+intro = '''
+# VOLVA PROJECT
+le projet a pour but d'analyser les historiques de volumes et de performance des différents 
+services d’un site logistique pour en déduire des tendances et développer un outil prévisionnel afin d’adapter les ressources humaines nécessaires aux opérations logistiques avec une projection à plusieurs semaines.
+Nous disposions des historiques de volumes de colis jour/jour sur la période 2020/2021, du nombre d’heures travaillées sur la même période et des notions de productivité par service (18 mois d’historique).
+ces données sont issues d'une requete Oracle realisé mensuellement.Nous avons donc dans un premier temps reuni l'ensemble des infos dans un seul dataset
+'''
