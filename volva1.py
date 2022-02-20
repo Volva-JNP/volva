c4980070c392bf4b9b0d34d4d79cddba2f5a523d
Volva-JNP <100067154+Volva-JNP@users.noreply.github.com>
2022-02-20 12:30:42 +0100

    Update volva1.py

--- volva1.py
+++ volva1.py
@@ -13,8 +13,9 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import plotly.express as px
 import plotly.figure_factory as ff
-from texts.volva_text import *
 from func.volva_fct import *
+from texts.volva_text import *
+
 
 
 
