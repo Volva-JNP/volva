789b83143b240df96f0a800eace6855307467384
Volva-JNP <100067154+Volva-JNP@users.noreply.github.com>
2022-02-20 14:08:25 +0100

    Add files via upload

--- texts/volva_text.py
+++ texts/volva_text.py
@@ -6,11 +6,28 @@ Created on Sat Feb 19 19:03:22 2022
 """
 
 
+
 intro = '''
 # VOLVA PROJECT
-le projet a pour but d'analyser les historiques de volumes et de performance des différents 
+Le projet a pour but d'analyser les historiques de volumes et de performance des différents 
 services d’un site logistique pour en déduire des tendances et développer un outil prévisionnel afin d’adapter les ressources humaines nécessaires aux opérations logistiques avec une projection à plusieurs semaines.
 Nous disposions des historiques de volumes de colis jour/jour sur la période 2020/2021, du nombre d’heures travaillées sur la même période et des notions de productivité par service (18 mois d’historique).
 ces données sont issues d'une requete Oracle realisé mensuellement.Nous avons donc dans un premier temps reuni l'ensemble des infos dans un seul dataset
 '''
 
+
+
+mobile = '''
+Ci-dessous un graphique representant les volumes de l'entrepot par secteur ainsi que le volume total sur les années 2020 et 2021
+'''
+
+
+
+violo = '''
+Représentation graphique des volumes journaliers avec distribution et ecart-type
+'''
+
+
+impact = '''
+Les graphiques ci-dessous representent la variation des volumes la semaine precedant le jour ferié en fonction de la place du jour férié dans la semaine
+'''
