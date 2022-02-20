ef9302fde55b1d1fcd9d78f15dfc0df094cfa2d4
Volva-JNP <100067154+Volva-JNP@users.noreply.github.com>
2022-02-20 11:13:06 +0100

    Update volva1.py

--- volva1.py
+++ volva1.py
@@ -9,6 +9,7 @@ import streamlit as st
 import numpy as np
 import streamlit as st
 import pandas as pd
+import matplotlib.pyplot as plt
 import seaborn as sns
 import plotly.express as px
 import plotly.figure_factory as ff
