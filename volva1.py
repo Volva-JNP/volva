8dc3eb02d8af41ef946425ee0ee7e2458411f222
Volva-JNP <100067154+Volva-JNP@users.noreply.github.com>
2022-02-20 10:05:02 +0100

    Update volva1.py

--- volva1.py
+++ volva1.py
@@ -1,1 +1,58 @@
-print("Ok")
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb 19 17:12:33 2022
+
+@author: User
+"""
+
+import streamlit as st
+import numpy as np
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import plotly.express as px
+import plotly.figure_factory as ff
+import volva_text
+from volva_fct import *
+
+
+
+
+
+
+
+st.sidebar.image('Les_Mousquetaires.png')
+st.sidebar.write("")
+
+
+
+
+#menu selection du dataset
+
+
+st.sidebar.header('MENU')
+st.sidebar.markdown('analyse de données ITM SQF')
+
+
+menu = st.sidebar.radio(
+    "",
+    ("Intro", "Data", "visualisation", 'model', "prediction"),
+)
+
+if menu == 'Intro':
+    set_home()  
+elif menu == 'Data':
+    set_data()
+elif menu == 'visualisation':
+    set_visu()
+elif menu == 'model':
+    set_model()
+elif menu == 'prediction':
+    set_pred()
+
+
+
+
+
+
