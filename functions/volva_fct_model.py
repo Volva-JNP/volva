d5da9a1d0fcc6596356b6df171a675f5db733c53
Philippe ARRIVE <parrive@macif.fr>
2022-02-21 21:02:23 +0100

    Test 3

--- functions/volva_fct_model.py
+++ functions/volva_fct_model.py
@@ -191,9 +191,10 @@ def build_list_test(df_FPTV, df_min, df_F, df_P, df_V, df_T):
 
     
 def build_df_datas_choice(list_nom_df, list_df, secteur):
-
+    st.write('3.1')
+    
     results = pd.DataFrame(columns=['Nom', 'Train_score', 'Test_score', 'Ecart'])
-
+    st.write('3.2')
     for nom_df, df,i in zip(list_nom_df,list_df, stqdm(range(16))) : 
 
         gridcv_GRB, X_train_scaled, X_test_scaled, y_train, y_test =  train_model(df,GBR,params_gbr,secteur)  
