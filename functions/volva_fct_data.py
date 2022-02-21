2472480bbd7e52ae9e677b8051e69a7fa6b3d1f7
Volva-JNP <100067154+Volva-JNP@users.noreply.github.com>
2022-02-21 10:07:47 +0100

    Update volva_fct_data.py

--- functions/volva_fct_data.py
+++ functions/volva_fct_data.py
@@ -30,31 +30,54 @@ path_brut = 'datas/volumesMARS2021.csv'
 def set_data():
     df = load_csv(path)
     st.title('Data')
-    st.write('select dataframe:')
     
     
-    st.write('<style>div.row-widget.stRadio > div{flex-direction:row;}</style>', unsafe_allow_html=True)
-    
-    menu = st.radio(
-    "",
-    ("secteur frais", "secteur Gel", "secteur FFL"),
-)
-    if menu =='secteur frais':
-        df = df.drop(['REALISE_TOTAL_GEL','REALISE_TOTAL_FFL'], axis = 1)
-        secteur = 'REALISE_TOTAL_FRAIS'
-        
-    
-    if menu == 'secteur Gel':
-        df = df.drop(['REALISE_TOTAL_FRAIS','REALISE_TOTAL_FFL'], axis = 1)
-        secteur = 'REALISE_TOTAL_GEL'
-        
-        
-    if menu == 'secteur FFL':
-        df = df.drop(['REALISE_TOTAL_GEL','REALISE_TOTAL_FRAIS'], axis = 1)
-        secteur = 'REALISE_TOTAL_FFL'
-        
+    col1, col2 = st.columns(2)
+    with col1:
+        st.write("Exemple d'un fichier mensuel")
+        st.write("")
+        st.write("")
+        st.write("")
+        st.write("")
+        st.write("")
+        st.write("")
+        st.image('img/dataori.jpg')
             
+            
+    with col2:
+        st.write('select dataframe:')
+            
+            
+        st.write('<style>div.row-widget.stRadio > div{flex-direction:row;}</style>', unsafe_allow_html=True)
+            
+        menu = st.radio(
+        "",
+        ("secteur frais", "secteur Gel", "secteur FFL"),
+        )
+        if menu =='secteur frais':
+            df = df.drop(['REALISE_TOTAL_GEL','REALISE_TOTAL_FFL'], axis = 1)
+            secteur = 'REALISE_TOTAL_FRAIS'
+            st.write(df[['DATE',secteur]])    
+            
+        if menu == 'secteur Gel':
+            df = df.drop(['REALISE_TOTAL_FRAIS','REALISE_TOTAL_FFL'], axis = 1)
+            secteur = 'REALISE_TOTAL_GEL'
+            st.write(df[['DATE',secteur]])    
+                
+        if menu == 'secteur FFL':
+            df = df.drop(['REALISE_TOTAL_GEL','REALISE_TOTAL_FRAIS'], axis = 1)
+            secteur = 'REALISE_TOTAL_FFL'
     
-    st.write(df[['DATE',secteur]])
+            st.write(df[['DATE',secteur]])
     
     st.write('<style>div.column-widget.stRadio > div{flex-direction:column;justify-content: center;} </style>', unsafe_allow_html=True)
+    
+    
+    #explication des données
+    
+    st.write("Explications des données et rapport d'exploration")
+    
+    st.write(DATA, unsafe_allow_html=True)
+    
+    
+    
