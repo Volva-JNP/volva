74858d6c470b4461f7e2afa143428bf7fbb21055
Volva Project <volva.project@gmail.com>
2022-03-01 22:03:56 +0100

    Amélioration du texte de data

--- texts/volva_text.py
+++ texts/volva_text.py
@@ -33,7 +33,12 @@ Chaque fichier représente un mois.
 
 Les données ont été nettoyées et adaptées à l'exploitation pour un analyse, visualisation, modelisation et prédiction.
 
+
 En fonction des hypothèses émises pour expliquer les variations de volume nous avons ajouté d'autres données externes à celles de départ:
+- position du jour dans l'espace temporel : Est-ce que la position du jour ou semaine dans le mois ou année impact le volume de commande?\n
+-> Nous avons donc intégré la position du jour dans la semaine, dans le mois et dans l'année ainsi que la position de la semaine dans le mois et dans l'année.
+
+
 - jours fériés : Est-ce que la proximité d'un jour férié (passé ou à venir) impacte les commandes de supermarchés pour compenser le jour de fermeture ? 
 -> Nous avons identifé si le jour férié le plus proche est passé ou à venir.
 
