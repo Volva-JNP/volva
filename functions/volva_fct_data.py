59d3d15745e7a3667b9c31aac8126669597ecff9
Volva-JNP <100067154+Volva-JNP@users.noreply.github.com>
2022-03-03 22:11:39 +0100

    Update volva_fct_data.py

--- functions/volva_fct_data.py
+++ functions/volva_fct_data.py
@@ -17,14 +17,14 @@ from plotly import tools
 from texts.volva_text import *
 from plotly.subplots import make_subplots
 from functions.volva_fct import *
-from diagrams import Cluster, Diagram
-from diagrams.k8s.storage import PVC, PV
-from diagrams.aws.database import Aurora
-from diagrams.gcp.iot import IotCore
-from diagrams.gcp.analytics import BigQuery, Dataflow, PubSub
-from diagrams.gcp.database import BigTable
-from diagrams.gcp.compute import AppEngine, Functions
-from diagrams.aws.database import Redshift, ElastiCache
+# from diagrams import Cluster, Diagram
+# from diagrams.k8s.storage import PVC, PV
+# from diagrams.aws.database import Aurora
+# from diagrams.gcp.iot import IotCore
+# from diagrams.gcp.analytics import BigQuery, Dataflow, PubSub
+# from diagrams.gcp.database import BigTable
+# from diagrams.gcp.compute import AppEngine, Functions
+# from diagrams.aws.database import Redshift, ElastiCache
 
 
 path = 'datas/volva_datas_utlimate_one.csv'
@@ -90,66 +90,66 @@ def set_data():
 #     st.image("img/flowchart.jpg",width=1400)
 
 
-# def flowchart():
-#   with Diagram("Construction du dataset final", outformat="jpg", filename="img/flowchart"):
+def flowchart():
+  with Diagram("Construction du dataset final", outformat="jpg", filename="img/flowchart"):
     
-#     with Cluster("DataSet Original"):
-#         with Cluster("Concatenation des mois"):
-#             dsoriginal_group = [PVC("mois 1"),
-#                                 PVC("mois 2"),
-#                                 PVC("mois 3"),
-#                                 PVC("mois...")]
-#         dsconcat = BigTable("DataSet Concaténé")
-#         with Cluster("Nettoyage des données"):
-#             dscleaning = [IotCore("Sélection des variables"),
-#                           IotCore("Traitement des NA"),
-#                           IotCore("Traitement des outliers"),
-#                           IotCore("Recherche des données manquantes")]
-#         dsfullclean = Aurora("DataSet nettoyé")
+    with Cluster("DataSet Original"):
+        with Cluster("Concatenation des mois"):
+            dsoriginal_group = [PVC("mois 1"),
+                                PVC("mois 2"),
+                                PVC("mois 3"),
+                                PVC("mois...")]
+        dsconcat = BigTable("DataSet Concaténé")
+        with Cluster("Nettoyage des données"):
+            dscleaning = [IotCore("Sélection des variables"),
+                          IotCore("Traitement des NA"),
+                          IotCore("Traitement des outliers"),
+                          IotCore("Recherche des données manquantes")]
+        dsfullclean = Aurora("DataSet nettoyé")
         
         
-#     with Cluster("Intégration de données supplémentaires"):
-#         inthyp = Dataflow("Intégration des hypothèses")
-# #         with Cluster("Espace temps"):
-# #             flow >> Functions("La position du jour influe-t-elle sur les volumes ?") >> Redshift("Variables jours/semaines dans espace temps")
-# #         with Cluster("Jours fériés"):
-# #             flow >> Functions("La proximité d'un jour férié (passé ou à venir) influe-t-elle sur les volumes ?") >> Redshift("Variables prox jour férié")
-# #         with Cluster("Vacances scolaires"):
-# #             flow >> Functions("Les vacances scolaires (par zone) influecent-elles les volumes ?") >> Redshift("Variables vacances")
-# #         with Cluster("Températures moyennes saisonnières"):
-# #             flow >> Functions("Les températures des régions de livraison influencent-elles les volumes ?") >> Redshift("Variables températures")
-# #         with Cluster("Promotions"):
-# #             flow >> Functions("Les promotions influencent-elles les volumes ?") >> Redshift("Variables promotions")
-# #         with Cluster("Semaines spéciales"):
-# #             flow >> Functions("Les semaines spéciales influencent-elles les volumes ?") >> Redshift("Variables semaines spéciales")
+    with Cluster("Intégration de données supplémentaires"):
+        inthyp = Dataflow("Intégration des hypothèses")
 #         with Cluster("Espace temps"):
-#             esptemp = inthyp >> Functions() >> Redshift()
+#             flow >> Functions("La position du jour influe-t-elle sur les volumes ?") >> Redshift("Variables jours/semaines dans espace temps")
 #         with Cluster("Jours fériés"):
-#             jferies = inthyp >> Functions() >> Redshift()
+#             flow >> Functions("La proximité d'un jour férié (passé ou à venir) influe-t-elle sur les volumes ?") >> Redshift("Variables prox jour férié")
 #         with Cluster("Vacances scolaires"):
-#             vac = inthyp >> Functions() >> Redshift()
+#             flow >> Functions("Les vacances scolaires (par zone) influecent-elles les volumes ?") >> Redshift("Variables vacances")
 #         with Cluster("Températures moyennes saisonnières"):
-#             temp = inthyp >> Functions() >> Redshift()
+#             flow >> Functions("Les températures des régions de livraison influencent-elles les volumes ?") >> Redshift("Variables températures")
 #         with Cluster("Promotions"):
-#             prom = inthyp >> Functions() >> Redshift()
+#             flow >> Functions("Les promotions influencent-elles les volumes ?") >> Redshift("Variables promotions")
 #         with Cluster("Semaines spéciales"):
-#             sem = inthyp >> Functions() >> Redshift()
+#             flow >> Functions("Les semaines spéciales influencent-elles les volumes ?") >> Redshift("Variables semaines spéciales")
+        with Cluster("Espace temps"):
+            esptemp = inthyp >> Functions() >> Redshift()
+        with Cluster("Jours fériés"):
+            jferies = inthyp >> Functions() >> Redshift()
+        with Cluster("Vacances scolaires"):
+            vac = inthyp >> Functions() >> Redshift()
+        with Cluster("Températures moyennes saisonnières"):
+            temp = inthyp >> Functions() >> Redshift()
+        with Cluster("Promotions"):
+            prom = inthyp >> Functions() >> Redshift()
+        with Cluster("Semaines spéciales"):
+            sem = inthyp >> Functions() >> Redshift()
 
         
-#     dsfinal = ElastiCache("DS final")
+    dsfinal = ElastiCache("DS final")
     
-#     dsanalyses = Redshift("Visualisation et analyses")
+    dsanalyses = Redshift("Visualisation et analyses")
     
-#     dsoriginal_group >> dsconcat >> dscleaning >> dsfullclean >> inthyp 
-#     esptemp >> dsfinal
-#     jferies >> dsfinal
-#     vac >> dsfinal
-#     temp >> dsfinal
-#     prom >> dsfinal
-#     sem >> dsfinal
+    dsoriginal_group >> dsconcat >> dscleaning >> dsfullclean >> inthyp 
+    esptemp >> dsfinal
+    jferies >> dsfinal
+    vac >> dsfinal
+    temp >> dsfinal
+    prom >> dsfinal
+    sem >> dsfinal
     
     
-#     dsfinal >> dsanalyses
+    dsfinal >> dsanalyses
 
 
     
