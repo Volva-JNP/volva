d978846e0dd56661aa9e482700e347a02eef88f5
Philippe ARRIVE <parrive@macif.fr>
2022-03-16 18:39:22 +0100

    add datas - Corrections

--- functions/volva_fct_add_datas.py
+++ functions/volva_fct_add_datas.py
@@ -9,17 +9,25 @@ from math import *
 from joblib import dump
 from calendar import monthrange
 from functions.utils.functions import *
+from datetime import date
+import shutil
 
 def build_page_add_datas():
     # redim_df()
+
+    volva_dataset = 'volva_datas_utlimate_one.csv'
+    path_datas = 'datas/'
+    path_datas_archives = path_datas + 'archives/'
+    
+
     uploaded_file = st.file_uploader("Choisissez un fichier")
     if uploaded_file is not None:
-        uploaded = pd.read_excel(uploaded_file, header=0)
-        volva_datas_utlimate_one = pd.read_csv('datas/volva_datas_utlimate_one2.csv', header=0)
+        uploaded = pd.read_excel(uploaded_file,  header=0)
+        volva_datas_utlimate_one = pd.read_csv(path_datas + volva_dataset, header=0)
 
         ordre_colonnes = volva_datas_utlimate_one.columns
 
-        if len(uploaded.columns) == 4 and \
+        if len(uploaded.columns) == 4   and \
             "DATE" in uploaded.columns  and \
             "REALISE_TOTAL_FRAIS" in uploaded.columns  and \
             "REALISE_TOTAL_GEL" in uploaded.columns  and \
@@ -56,7 +64,13 @@ def build_page_add_datas():
                     result_addition = pd.concat([volva_datas_utlimate_one,df_added_datas])
                     result_addition=result_addition.reset_index()
                     st.write(result_addition)
-                
+                    save_dataset = st.button("Sauvegarder le nouveau dataset")
+                    if save_dataset:
+                        today = date.today()
+                        volva_dataset_path = path_datas + volva_dataset
+                        volva_dataset_archives_path = path_datas_archives + str(today) + ' - ' + volva_dataset
+                        shutil.copyfile(volva_dataset_path, volva_dataset_archives_path)
+                        result_addition.to_csv(volva_dataset_path)
 
 
         else:
@@ -67,7 +81,9 @@ def build_page_add_datas():
                         "- REALISE_TOTAL_FRAIS  \n"\
                         "- REALISE_TOTAL_GEL  \n"\
                         "- REALISE_TOTAL_FFL  \n"\
-                        + str(len(uploaded.columns))                            
+                        + str(uploaded.columns)                            
             )
+
+            st.write(uploaded)
             
     
