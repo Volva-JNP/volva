598953cb8bfdb59cab498d18fa2930fcf7c6be3e
JK <jkhenniche@gmail.com>
2022-04-03 21:18:36 +0200

    photo4

--- app.py
+++ app.py
@@ -140,12 +140,12 @@ st.sidebar.write("")
 st.sidebar.markdown(f'<center><u>Volva Team</u></center>', unsafe_allow_html=True)
 st.sidebar.write("")
 link='[Phil Arrive](https://www.linkedin.com/in/philippe-arrive-954765137/)'
-st.sidebar.image("img/Image4.jpg")
+st.sidebar.image("img/Image4.jpg",width=50)
 st.sidebar.markdown(link,unsafe_allow_html= True )
 
 
 link='[Nicolas Francois](https://www.linkedin.com/in/nicolas-francois-finance-and-bi/)'
-st.sidebar.image("img/Image6.jpg")
+st.sidebar.image("img/Image6.jpg",width=50)
 st.sidebar.markdown(link,unsafe_allow_html= True )
 
 link='[Julien Khenniche](https://www.linkedin.com/in/philippe-arrive-954765137/)'
