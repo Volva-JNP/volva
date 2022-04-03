df9500a11887412b887b912e47fd8053f49f201a
JK <jkhenniche@gmail.com>
2022-04-03 21:56:08 +0200

    photonon centré

--- app.py
+++ app.py
@@ -139,17 +139,19 @@ st.sidebar.markdown(f'<center>created by :</center>', unsafe_allow_html=True)
 st.sidebar.write("")
 st.sidebar.markdown(f'<center><u>Volva Team</u></center>', unsafe_allow_html=True)
 st.sidebar.write("")
+st.sidebar.image("img/Image4.jpg",width=50)
 link='[Phil Arrive](https://www.linkedin.com/in/philippe-arrive-954765137/)'
 st.sidebar.markdown(link,unsafe_allow_html= True )
-st.sidebar.image("img/Image4.jpg",width=50)
 
+
+st.sidebar.image("img/Image6.jpg",width=50)
 link='[Nicolas Francois](https://www.linkedin.com/in/nicolas-francois-finance-and-bi/)'
 st.sidebar.markdown(link,unsafe_allow_html= True )
-st.sidebar.image("img/Image6.jpg",width=50)
 
+st.sidebar.image('img/Image5.jpg',width=50,)
 link='[Julien Khenniche](https://www.linkedin.com/in/philippe-arrive-954765137/)'
 st.sidebar.markdown(link,unsafe_allow_html= True )
-st.sidebar.image('img/Image5.jpg',width=50)
+
 
 st.sidebar.write("")
 st.sidebar.markdown(f'<center>and</center>', unsafe_allow_html=True)
